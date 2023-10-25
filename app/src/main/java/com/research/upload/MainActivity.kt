32c2cc6dd525871277dfdf4f91173f90074f5171
Fakhry Mubarak <fakhrymubarak@gmail.com>
2023-10-25 15:49:05 +0700

    add document picker

--- app/src/main/java/com/research/upload/MainActivity.kt
+++ app/src/main/java/com/research/upload/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.research.upload
 
+import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
@@ -15,13 +19,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val pickDocumentLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val uri = result.data?.data
+            if (uri == null) {
+                Log.d("DocumentPicker", "No document selected")
+                return@registerForActivityResult
+            }
 
-    /**
-     * Compatible to Android 11 (API Level 30) or later.
-     * Need to add some code in manifest to enable backported installation through Google Play Service.
-     * @see <a href="https://developer.android.com/training/data-storage/shared/photopicker#device-availability.">Device Compatibility</a>
-     *
-     * */
+            Log.d("DocumentPicker", "Selected URI: $uri")
+            uploadDocumentToServer(uri)
+        }
+
     private val pickMedia =
         registerForActivityResult(PickVisualMedia()) { uri ->
             if (uri == null) {
@@ -32,15 +41,6 @@ class MainActivity : AppCompatActivity() {
             Log.d("PhotoPicker", "Selected URI: $uri")
             uploadImageToServer(uri)
         }
-
-    private fun uploadImageToServer(uri: Uri) {
-        val imageFiles = File(uri.path ?: "")
-        Log.d("PhotoPicker", "Selected URI: $imageFiles")
-
-        // viewModel.uploadImage(imagePath)
-
-        binding.ivUploadedImage.load(uri)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +53,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun initEvent() {
         with(binding) {
+            btnUploadFiles.setOnClickListener {
+                pickDocument()
+            }
+
             btnUploadImages.setOnClickListener {
                 pickImage()
             }
@@ -64,9 +68,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun pickDocument() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, "")
+            }
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        }
+        pickDocumentLauncher.launch(intent)
+    }
 
     private fun pickImage() {
         // Launch the photo picker and let the user choose only images.
         pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+    }
+
+
+    private fun uploadDocumentToServer(uri: Uri) {
+        val documentFiles = File(uri.path ?: "")
+        Log.d("DocumentPicker", "Selected URI: $documentFiles")
+
+        // viewModel.uploadImage(documentFiles)
+
+    }
+
+    private fun uploadImageToServer(uri: Uri) {
+        val imageFiles = File(uri.path ?: "")
+        Log.d("PhotoPicker", "Selected URI: $imageFiles")
+
+        // viewModel.uploadImage(imagePath)
+        binding.ivUploadedImage.load(uri)
     }
 }
